a173636f9369603429ad191ccda5f089fa6a6dd1
carbonbasedi <carbonbasedai@gmail.com>
2023-07-21 19:56:46 +0400

    typo in accountcontroller fixed :D

--- Presentation/Controllers/AccountController.cs
+++ Presentation/Controllers/AccountController.cs
@@ -2,13 +2,15 @@
 
 namespace Presentation.Controllers
 {
-	public class AccountContoller : Controller
+	public class AccountController : Controller
 	{
+		[HttpGet]
 		public IActionResult Login()
 		{
 			return View();
 		}
 
+		[HttpGet]
 		public IActionResult Register()
 		{
 			return View();
